51da3ca653fb45b404ae85ca9f8ec20518d03b95
Roger Leigh <rleigh@codelibre.net>
2018-02-03 11:50:28 +0000

    Remove ant build

--- tools/test-build.bat
+++ tools/test-build.bat
@@ -9,17 +9,3 @@ if [%build%] == [maven] (
   REM Test the maven build
   mvn install || exit /b 1
 )
-
-if [%build%] == [ant] (
-  REM Test the ant build
-  ant clean compile || exit /b 1
-  ant clean compile-autogen || exit /b 1
-  ant clean compile-formats-api || exit /b 1
-  ant clean compile-bio-formats-plugins || exit /b 1
-  ant clean compile-formats-bsd || exit /b 1
-  ant clean compile-formats-gpl || exit /b 1
-  ant clean compile-bio-formats-tools || exit /b 1
-  ant clean compile-turbojpeg || exit /b 1
-  ant clean utils || exit /b 1
-  ant -Dsphinx.warnopts="-W" clean-docs-sphinx docs-sphinx || exit /b 1
-)
